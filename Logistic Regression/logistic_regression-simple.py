baf251753f33fe0760c34ab11ef21535faa7d6e7
Aryan Mehta <43165103+aryan-mehta@users.noreply.github.com>
2020-10-31 23:16:49 +0530

    Add files via upload

---
+++ Logistic Regression/logistic_regression-simple.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 29 08:18:52 2019
+
+@author: 17BIS0091
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from utilities import visualize_classifier
+#mapping something which is continuous v/s which is discrete
+X=np.array([[3.1,7.2],[4,6.7],[2.9,8],[5.1,4.5],[6,5],[5.6,5],[3.3,0.4],[3.9,0.9],
+           [2.8,1],[0.5,3.4],[1,4],[0.6,4.9]])
+y=np.array([0,0,0,1,1,1,2,2,2,3,3,3])
+
+#classifier=linear_model.LogisticRegression(solver="liblinear",C=1)
+classifier=linear_model.LogisticRegression(solver="liblinear",C=100)
+
+classifier.fit(X,y)
+
+visualize_classifier(classifier,X,y)
